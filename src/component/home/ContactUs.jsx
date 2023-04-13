12470deb71ecc668b9837bbd916a13004966bc07
Abhinav-Skil <109666018+Abhinav-Skil@users.noreply.github.com>
2023-04-13 22:30:10 +0530

    home.contact

--- src/component/home/ContactUs.jsx
+++ src/component/home/ContactUs.jsx
@@ -4,7 +4,7 @@ import Contact from "./ContactUs/Contact";
 export default function ContactUs() {
   return (
     <div className=" ">
-      <div className=" xs:bg-bg">
+      <div className=" bg-bg">
         <div className="gradient_leftTop mgradient_rightBot">
           <div className="text-white mgradient_rightTopSmall gradient_rightBot text-left space-y-8 p-8 sm:p-28 sm:pt-36 sm:space-y-8 ">
             <h1 className="maintitlebkbt sm:max-w-xl">
